1215d0ff3fbb25e1baa85fe5ff21c134161fe8f0
NxtGenLabs <nxtgentab@gmail.com>
2022-06-22 15:36:25 +0200

    fixed toolbar for transform intro : rotation and scale

--- lib/pages/topics/transformations/transformation_intro.dart
+++ lib/pages/topics/transformations/transformation_intro.dart
@@ -4,7 +4,6 @@ import 'dart:math' as math;
 import '../../../canvas/grid.dart';
 import '../../../canvas/transformation_intro_painter.dart';
 
-
 class TransformIntro extends StatefulWidget {
   @override
   _TransformIntroState createState() => _TransformIntroState();
@@ -22,62 +21,74 @@ class _TransformIntroState extends State<TransformIntro> {
         title: Text('Polygons'),
       ),
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Expanded(
-              child: CustomPaint(
-                painter: ShapePainter(_sides, _radius, _radians),
-                foregroundPainter: MyGridPainter(),
-                child: Container(),
-              ),
+        child: Stack(children: [
+          Expanded(
+            child: CustomPaint(
+              foregroundPainter: ShapePainter(_sides, _radius, _radians),
+              painter: MyGridPainter(),
+              child: Container(),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Text('Sides'),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.grey,
+                  height: 220,
+                  width: MediaQuery.of(context).size.width / 2,
+                  child: Column(children: [
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Text('Sides'),
+                    ),
+                    Slider(
+                      value: _sides,
+                      min: 3.0,
+                      max: 10.0,
+                      label: _sides.toInt().toString(),
+                      divisions: 7,
+                      onChanged: (value) {
+                        setState(() {
+                          _sides = value;
+                        });
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Text('Size'),
+                    ),
+                    Slider(
+                      value: _radius,
+                      min: 10.0,
+                      max: MediaQuery.of(context).size.width / 2,
+                      onChanged: (value) {
+                        setState(() {
+                          _radius = value;
+                        });
+                      },
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Text('Rotation'),
+                    ),
+                    Slider(
+                      value: _radians,
+                      min: 0.0,
+                      max: math.pi,
+                      onChanged: (value) {
+                        setState(() {
+                          _radians = value;
+                        });
+                      },
+                    ),
+                  ]),
+                ),
+              ],
             ),
-            Slider(
-              value: _sides,
-              min: 3.0,
-              max: 10.0,
-              label: _sides.toInt().toString(),
-              divisions: 7,
-              onChanged: (value) {
-                setState(() {
-                  _sides = value;
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Text('Size'),
-            ),
-            Slider(
-              value: _radius,
-              min: 10.0,
-              max: MediaQuery.of(context).size.width / 2,
-              onChanged: (value) {
-                setState(() {
-                  _radius = value;
-                });
-              },
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 16.0),
-              child: Text('Rotation'),
-            ),
-            Slider(
-              value: _radians,
-              min: 0.0, 
-              max: math.pi,
-              onChanged: (value) {
-                setState(() {
-                  _radians = value;
-                });
-              },
-            ),
-          ],
-        ),
+          )
+        ]),
       ),
     );
   }
