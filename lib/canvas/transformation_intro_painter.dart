407eaa1c5d012ad1c2b248a90c1301e4fe5447c6
NxtGenLabs <nxtgentab@gmail.com>
2022-06-23 15:05:56 +0200

    Added the angle method: Finding the angle between two 2d vectors

--- lib/canvas/transformation_intro_painter.dart
+++ lib/canvas/transformation_intro_painter.dart
@@ -3,13 +3,23 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-
 // FOR PAINTING POLYGONS
 class ShapePainter extends CustomPainter {
   final double sides;
   final double radius;
   final double radians;
   ShapePainter(this.sides, this.radius, this.radians);
+
+  // angle calculator method
+  calcAngle(Offset v1, Offset v2) {
+    var vecDotProduct = (v1.dx * v2.dx) + (v1.dy * v2.dy); // the dotproduct of 2d vectors
+    var magVec1 = math.sqrt(math.pow(v1.dx, 2) + math.pow(v1.dy, 2)); // magnitude of vector 1
+    var magVec2 = math.sqrt(math.pow(v2.dx, 2) + math.pow(v2.dy, 2)); // magnitude of vector 2
+    var magProduct = magVec1 * magVec2;
+
+    var angle = math.acos(vecDotProduct / magProduct);
+    return angle;
+  }
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -35,6 +45,12 @@ class ShapePainter extends CustomPainter {
     }
     path.close();
     canvas.drawPath(path, paint);
+
+    // displaying angle
+    TextSpan span = TextSpan(style: TextStyle(color: Colors.red[900]), text: '${Offset(0, 0)}');
+    TextPainter tp = TextPainter(text: span, textAlign: TextAlign.left, textDirection: TextDirection.ltr, textScaleFactor: .8);
+    tp.layout();
+    tp.paint(canvas, Offset(size.width / 2, size.height / 2));
   }
 
   @override
