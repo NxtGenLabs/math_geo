bffd5199fe80cbf8293eb3e888a052fe284c39d8
Mwayi Kamvantope <kamvantope.mfs.official01@gmail.com>
2022-08-28 16:01:07 +0200

    added the tutorial function to all in one training station page

--- lib/pages/classes/Form 3/subjects/Mathematics/topics/transformations/level/trainingStation/training_station.dart
+++ lib/pages/classes/Form 3/subjects/Mathematics/topics/transformations/level/trainingStation/training_station.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'dart:math' as math;
 // user defiend imports
 import 'package:math_geometry/canvas/grid.dart';
-import 'package:math_geometry/canvas/transformation_intro_painter.dart';
 
-import '../../../../../../../../../widgets/customAppbar.dart';
+import 'package:math_geometry/widgets/customAppbar.dart';
+import 'package:math_geometry/widgets/customDialog.dart';
 
 class TrainingStation extends StatefulWidget {
   const TrainingStation({Key? key}) : super(key: key);
@@ -14,10 +13,62 @@ class TrainingStation extends StatefulWidget {
 }
 
 class _TrainingStationState extends State<TrainingStation> {
-  var _sides = 3.0;
-  var _radius = 100.0;
-  var _radians = 0.0;
-  var _position = 0.0;
+  //!
+  List<CustomDialog> returnTutorials() {
+    List<CustomDialog> tutorial = [
+      CustomDialog(
+        title: 'title1',
+        clsBtnTitle: 'Next',
+        message: 'message',
+        attempt: false,
+        header: Icons.lightbulb_circle_outlined,
+        headerColor: Colors.yellow,
+        onClsBtnPressed: () => Navigator.of(context).pop(),
+      ),
+      CustomDialog(
+        title: 'title2',
+        clsBtnTitle: 'Next',
+        message: 'message',
+        attempt: false,
+        header: Icons.lightbulb_circle_outlined,
+        headerColor: Colors.yellow,
+        onClsBtnPressed: () => Navigator.of(context).pop(),
+      ),
+      CustomDialog(
+        title: 'title3',
+        clsBtnTitle: 'Done.',
+        message: 'message',
+        attempt: false,
+        header: Icons.lightbulb_circle_outlined,
+        headerColor: Colors.yellow,
+        onClsBtnPressed: () => Navigator.of(context).pop(),
+      ),
+    ];
+    return tutorial;
+  }
+
+  int tutorialIndex = 0;
+  late List<CustomDialog> tutorial = returnTutorials();
+
+  void showTutorial(List arr, int index) async {
+    if (index >= arr.length) {
+      return;
+    }
+    await showDialog(
+      barrierDismissible: false,
+      context: context,
+      builder: (_) {
+        return arr[index];
+      }, /*dialog code here with arr[index]*/
+    );
+    showTutorial(arr, index + 1);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // showTutorial(tutorial, tutorialIndex); // may need a future method
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,84 +79,8 @@ class _TrainingStationState extends State<TrainingStation> {
           Container(
             color: Colors.grey[400],
             child: CustomPaint(
-              foregroundPainter:
-                  ShapePainter(_sides, _radius, _radians, _position),
               painter: MyGridPainter(),
               child: Container(),
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.grey,
-                  height: 300,
-                  width: MediaQuery.of(context).size.width / 2,
-                  child: Column(children: [
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16.0, top: 10.0),
-                      child: Text('Sides'),
-                    ),
-                    Slider(
-                      value: _sides,
-                      min: 3.0,
-                      max: 10.0,
-                      label: _sides.toInt().toString(),
-                      divisions: 7,
-                      onChanged: (value) {
-                        setState(() {
-                          _sides = value;
-                        });
-                      },
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Text('Size'),
-                    ),
-                    Slider(
-                      value: _radius,
-                      min: 10.0,
-                      max: MediaQuery.of(context).size.width / 2,
-                      onChanged: (value) {
-                        setState(() {
-                          _radius = value;
-                        });
-                      },
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Text('Rotation'),
-                    ),
-                    Slider(
-                      value: _radians,
-                      min: 0.0,
-                      max: math.pi,
-                      onChanged: (value) {
-                        setState(() {
-                          _radians = value;
-                        });
-                      },
-                    ),
-                    const Padding(
-                      padding: EdgeInsets.only(left: 16.0),
-                      child: Text('Translate'),
-                    ),
-                    Slider(
-                      value: _position,
-                      min: 0.0,
-                      divisions: 10,
-                      max: 100,
-                      onChanged: (value) {
-                        setState(() {
-                          _position = value;
-                        });
-                      },
-                    ),
-                  ]),
-                ),
-              ],
             ),
           ),
           Row(
